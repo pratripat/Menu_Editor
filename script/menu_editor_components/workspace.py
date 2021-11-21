5b538cb8e5b51b69192c30c0a1fd1477ed7a11c3
Pratyush Ram Tripathi <pratyusht2006@gmail.com>
2021-11-21 15:56:54 +0530

    Added radiobuttons, loading system.
    Signed-off-by: Pratyush Ram Tripathi <pratyusht2006@gmail.com>

--- script/menu_editor_components/workspace.py
+++ script/menu_editor_components/workspace.py
@@ -4,16 +4,39 @@ from ..ux.menu import Menu
 class Workspace:
     def __init__(self, menu_editor):
         self.menu_editor = menu_editor
-        self.main_menu = Menu(self.menu_editor, json.load(open('data/configs/menus/main_menu.json', 'r')))
+        self.main_menu = self.menu_editor.menu_manager.get_menu_with_id('main_menu')
+        # self.main_menu = Menu(self.menu_editor, json.load(open('data/configs/menus/main_menu.json', 'r')))
         self.current_menu = self.main_menu
         self.start_position = None
 
         self.scroll = [0,0]
 
     def render(self):
-        self.current_menu.render(self.scroll)
+        # self.current_menu.render(self.scroll)
+        pass
 
     def update(self):
+        self.current_menu.update(self.scroll)
+
+        if self.current_menu.selected_object and self.current_menu.selected_object.object_id == 'textbox':
+            self.menu_editor.format_panel.update_attrs()
+        else:
+            self.update_scrolling()
+            if 'l' in self.menu_editor.keys_pressed:
+                self.menu_editor.load_open_dialogbox()
+        pass
+
+    def update_current_object(self):
+        for object in self.current_menu.objects:
+            if object.is_clicked(self.scroll):
+                self.set_current_object(object)
+                return
+
+        #TEMP
+        if self.current_menu.is_clicked(self.scroll):
+            self.set_current_object(self.current_menu)
+
+    def update_scrolling(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.scroll[0] -= 5 * self.menu_editor.fps * self.menu_editor.dt
@@ -23,23 +46,6 @@ class Workspace:
             self.scroll[1] -= 5 * self.menu_editor.fps * self.menu_editor.dt
         if keys[pygame.K_s]:
             self.scroll[1] += 5 * self.menu_editor.fps * self.menu_editor.dt
-
-        self.update_current_object()
-
-    def update_current_object(self):
-        self.current_menu.update(self.scroll)
-
-        if self.current_menu.selected_object and self.current_menu.selected_object.object_id == 'textbox':
-            self.menu_editor.format_panel.update_attrs()
-
-        for object in self.current_menu.objects:
-            if object.is_clicked(self.scroll):
-                self.set_current_object(object)
-                return
-
-        #TEMP
-        if self.current_menu.is_clicked(self.scroll):
-            self.set_current_object(self.current_menu)
 
     def add_object(self, position):
         if not self.start_position or not self.menu_editor.selection_panel.menu.selected_object:
@@ -53,11 +59,18 @@ class Workspace:
             object = self.current_menu.add_textbox(self.start_position, [position[0]+self.scroll[0], position[1]+self.scroll[1]])
         elif self.menu_editor.selection_panel.menu.selected_object.id == 'checkbox_button':
             object = self.current_menu.add_checkbox(self.start_position, [position[0]+self.scroll[0], position[1]+self.scroll[1]])
+        elif self.menu_editor.selection_panel.menu.selected_object.id == 'radiobutton_button':
+            object = self.current_menu.add_radiobutton(self.start_position, [position[0]+self.scroll[0], position[1]+self.scroll[1]])
 
         if object:
             self.set_current_object(object)
 
         self.start_position = None
+        pass
+
+    def load_data(self, filepath):
+        self.main_menu = Menu(self.menu_editor, json.load(open(filepath, 'r')))
+        self.current_menu = self.main_menu
 
     def set_current_object(self, object):
         self.current_object = object
